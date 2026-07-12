219ac87e503425f3d16daf07feb1ed142d6591b2
Shouvik Jha <58849426+jhashouvik@users.noreply.github.com>
2026-07-13 00:28:43 +0530

    Update diagram artifacts via Archly

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,65 @@
-# >>> archly:node:key_vault >>>
-output "key_vault_id" {
-  description = "Key Vault id (reference this as key_vault_id elsewhere)"
-  value       = azurerm_key_vault.key_vault.id
+
+
+# >>> archly:node:alb1 >>>
+output "alb1_dns_name" {
+  description = "Public DNS name of the load balancer"
+  value       = aws_lb.alb1.dns_name
   sensitive   = false
 }
-# <<< archly:node:key_vault <<<
+# <<< archly:node:alb1 <<<
+
+# >>> archly:node:dns1 >>>
+output "dns1_zone_id" {
+  description = "Route53 hosted zone id"
+  value       = aws_route53_zone.dns1.zone_id
+  sensitive   = false
+}
+# <<< archly:node:dns1 <<<
+
+# >>> archly:node:dynamodb1 >>>
+output "dynamodb1_arn" {
+  description = "Table ARN"
+  value       = aws_dynamodb_table.dynamodb1.arn
+  sensitive   = false
+}
+# <<< archly:node:dynamodb1 <<<
+
+# >>> archly:node:ec2_auto1 >>>
+output "ec2_auto1_id" {
+  description = "Instance id"
+  value       = aws_instance.ec2_auto1.id
+  sensitive   = false
+}
+# <<< archly:node:ec2_auto1 <<<
+
+# >>> archly:node:lambda1 >>>
+output "lambda1_arn" {
+  description = "Function ARN"
+  value       = aws_lambda_function.lambda1.arn
+  sensitive   = false
+}
+# <<< archly:node:lambda1 <<<
+
+# >>> archly:node:queue1 >>>
+output "queue1_url" {
+  description = "Queue URL"
+  value       = aws_sqs_queue.queue1.url
+  sensitive   = false
+}
+# <<< archly:node:queue1 <<<
+
+# >>> archly:node:rds1 >>>
+output "rds1_endpoint" {
+  description = "Connection endpoint"
+  value       = aws_db_instance.rds1.endpoint
+  sensitive   = false
+}
+# <<< archly:node:rds1 <<<
+
+# >>> archly:node:s3_images >>>
+output "s3_images_arn" {
+  description = "Bucket ARN"
+  value       = aws_s3_bucket.s3_images.arn
+  sensitive   = false
+}
+# <<< archly:node:s3_images <<<
