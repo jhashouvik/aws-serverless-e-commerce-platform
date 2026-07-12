ba7b3eb3fb6446afaf219e10513bbaa6f8e5b73a
Shouvik Jha <58849426+jhashouvik@users.noreply.github.com>
2026-07-13 00:48:10 +0530

    Update diagram artifacts via Archly

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,10 @@
-
+# >>> archly:node:dns1 >>>
+output "dns1_zone_id" {
+  description = "Route53 hosted zone id"
+  value       = aws_route53_zone.dns1.zone_id
+  sensitive   = false
+}
+# <<< archly:node:dns1 <<<
 
 # >>> archly:node:alb1 >>>
 output "alb1_dns_name" {
@@ -8,22 +14,6 @@ output "alb1_dns_name" {
 }
 # <<< archly:node:alb1 <<<
 
-# >>> archly:node:dns1 >>>
-output "dns1_zone_id" {
-  description = "Route53 hosted zone id"
-  value       = aws_route53_zone.dns1.zone_id
-  sensitive   = false
-}
-# <<< archly:node:dns1 <<<
-
-# >>> archly:node:dynamodb1 >>>
-output "dynamodb1_arn" {
-  description = "Table ARN"
-  value       = aws_dynamodb_table.dynamodb1.arn
-  sensitive   = false
-}
-# <<< archly:node:dynamodb1 <<<
-
 # >>> archly:node:ec2_auto1 >>>
 output "ec2_auto1_id" {
   description = "Instance id"
@@ -32,13 +22,13 @@ output "ec2_auto1_id" {
 }
 # <<< archly:node:ec2_auto1 <<<
 
-# >>> archly:node:lambda1 >>>
-output "lambda1_arn" {
-  description = "Function ARN"
-  value       = aws_lambda_function.lambda1.arn
+# >>> archly:node:rds1 >>>
+output "rds1_endpoint" {
+  description = "Connection endpoint"
+  value       = aws_db_instance.rds1.endpoint
   sensitive   = false
 }
-# <<< archly:node:lambda1 <<<
+# <<< archly:node:rds1 <<<
 
 # >>> archly:node:queue1 >>>
 output "queue1_url" {
@@ -48,13 +38,21 @@ output "queue1_url" {
 }
 # <<< archly:node:queue1 <<<
 
-# >>> archly:node:rds1 >>>
-output "rds1_endpoint" {
-  description = "Connection endpoint"
-  value       = aws_db_instance.rds1.endpoint
+# >>> archly:node:lambda1 >>>
+output "lambda1_arn" {
+  description = "Function ARN"
+  value       = aws_lambda_function.lambda1.arn
   sensitive   = false
 }
-# <<< archly:node:rds1 <<<
+# <<< archly:node:lambda1 <<<
+
+# >>> archly:node:dynamodb1 >>>
+output "dynamodb1_arn" {
+  description = "Table ARN"
+  value       = aws_dynamodb_table.dynamodb1.arn
+  sensitive   = false
+}
+# <<< archly:node:dynamodb1 <<<
 
 # >>> archly:node:s3_images >>>
 output "s3_images_arn" {
