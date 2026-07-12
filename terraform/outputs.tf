0a70464d259f8204e67c2692ec27b19671e741f2
Shouvik Jha <58849426+jhashouvik@users.noreply.github.com>
2026-07-13 01:03:52 +0530

    Update diagram artifacts via Archly

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -15,9 +15,9 @@ output "alb1_dns_name" {
 # <<< archly:node:alb1 <<<
 
 # >>> archly:node:ec2_auto1 >>>
-output "ec2_auto1_id" {
-  description = "Instance id"
-  value       = aws_instance.ec2_auto1.id
+output "ec2_auto1_arn" {
+  description = "Function ARN"
+  value       = aws_lambda_function.ec2_auto1.arn
   sensitive   = false
 }
 # <<< archly:node:ec2_auto1 <<<
